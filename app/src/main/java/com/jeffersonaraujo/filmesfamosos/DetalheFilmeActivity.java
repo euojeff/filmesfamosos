3318c8c51462fd5789f44fa273a45c5fd0194009
euojeff <jeff.daraujo@gmail.com>
2017-12-07 21:37:11 -0300

    finalizacao da tela de detalhe

--- app/src/main/java/com/jeffersonaraujo/filmesfamosos/DetalheFilmeActivity.java
+++ app/src/main/java/com/jeffersonaraujo/filmesfamosos/DetalheFilmeActivity.java
@@ -2,23 +2,73 @@ package com.jeffersonaraujo.filmesfamosos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.jeffersonaraujo.filmesfamosos.helpers.FilmeJsonHelper;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
 
 public class DetalheFilmeActivity extends AppCompatActivity {
 
     public final static String BUNDLE_JSON_FILME = "json_filme";
-    TextView detalhe;
+
+    private FilmeJsonHelper mhelper;
+
+    private ImageView cartazIV;
+    private ImageView backDropIV;
+    private TextView tituloTv;
+    private TextView tituloOriginalTv;
+    private TextView avaliacaoTv;
+    private TextView lancamentoTv;
+    private TextView sinopseTv;
+
+    private void preencherInformacoes(){
+        try {
+            Picasso.with(this).load(mhelper.getBackDrop()).into(backDropIV);
+            Picasso.with(this).load(mhelper.getPathCartaz()).into(cartazIV);
+
+            tituloTv.setText(mhelper.getTitulo());
+            tituloOriginalTv.setText(mhelper.getTituloOriginal());
+            avaliacaoTv.setText(mhelper.getAvaliacao() + "/10");
+            lancamentoTv.setText(mhelper.getDataLancamento());
+            sinopseTv.setText(mhelper.getSinopse());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe_filme);
 
-        detalhe = findViewById(R.id.detalhe);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        cartazIV = findViewById(R.id.img_miniatura_filme_detalhe);
+        backDropIV = findViewById(R.id.img_back_filme_detalhe);
+        tituloTv = findViewById(R.id.titulo_filme_detalhe);
+        tituloOriginalTv = findViewById(R.id.detalhe_titulo_original);
+        avaliacaoTv = findViewById(R.id.detalhe_avaliacao);
+        lancamentoTv = findViewById(R.id.detalhe_data_lancamento);
+        sinopseTv = findViewById(R.id.detalhe_sinopse);
 
         if(getIntent().hasExtra(BUNDLE_JSON_FILME)){
-            detalhe.setText(getIntent().getStringExtra(BUNDLE_JSON_FILME));
+            try {
+                mhelper = new FilmeJsonHelper(getIntent().getStringExtra(BUNDLE_JSON_FILME));
+                preencherInformacoes();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
--- app/src/main/java/com/jeffersonaraujo/filmesfamosos/helpers/FilmeJsonHelper.java
+++ app/src/main/java/com/jeffersonaraujo/filmesfamosos/helpers/FilmeJsonHelper.java
@@ -13,13 +13,39 @@ public class FilmeJsonHelper{
 
     private JSONObject mJson;
 
-    private static String IMAGE_ROOT = "http://image.tmdb.org/t/p/w185/";
+    private static String IMAGE_ROOT = "http://image.tmdb.org/t/p/";
+    private static String TAMANHO_MINIATURA = "w185/";
+    private static String TAMANHO_GRANDE = "w780/";
 
     public FilmeJsonHelper(String json) throws JSONException {
         this.mJson = new JSONObject(json);
     }
 
     public String getPathCartaz() throws JSONException {
-        return IMAGE_ROOT + mJson.getString("poster_path");
+        return IMAGE_ROOT + TAMANHO_MINIATURA + mJson.getString("poster_path");
+    }
+
+    public String getBackDrop() throws JSONException {
+        return IMAGE_ROOT + TAMANHO_GRANDE + mJson.getString("backdrop_path");
+    }
+
+    public String getTitulo() throws JSONException {
+        return mJson.getString("title");
+    }
+
+    public String getTituloOriginal() throws JSONException {
+        return mJson.getString("original_title");
+    }
+
+    public String getAvaliacao() throws JSONException {
+        return mJson.getString("vote_average");
+    }
+
+    public String getDataLancamento() throws JSONException {
+        return mJson.getString("release_date");
+    }
+
+    public String getSinopse() throws JSONException {
+        return mJson.getString("overview");
     }
 }
